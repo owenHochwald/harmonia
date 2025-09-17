b173d68bc397ff681545db8b647d567d39381a56
owenhochwald <owenhochwald@gmail.com>
2025-09-16 17:53:30 -0700

    add database connection

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
@@ -16,10 +19,19 @@ func main() {
 	if err != nil {
 		panic("Error loading .env file")
 	}
-	// TODO: add database connection
+
+	log := logger.NewLogger(os.Getenv("ENVIRONMENT"))
+	cfg := config.NewConfig()
+
+	db, err := ConnectDB(cfg)
+	if err != nil {
+		log.Fatal().Err(err).Msg("Cannot connect to database")
+	}
+
 	app := server.Application{
-		Logger: logger.NewLogger(os.Getenv("ENVIRONMENT")),
-		Config: config.NewConfig(),
+		Logger: log,
+		Config: cfg,
+		DB:     db,
 	}
 
 	r := gin.Default()
@@ -27,4 +39,26 @@ func main() {
 	server.SetupRoutes(r, &app)
 
 	r.Run(":" + app.Config.Port)
+}
+
+func ConnectDB(cfg config.Config) (*sql.DB, error) {
+	var err error
+	db, err := sql.Open("postgres", cfg.DBURL)
+
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	if err = db.PingContext(ctx); err != nil {
+		return nil, err
+	}
+
+	db.SetConnMaxLifetime(time.Hour)
+	db.SetMaxOpenConns(25)
+	db.SetConnMaxIdleTime(10 * time.Minute)
+
+	return db, nil
 }
