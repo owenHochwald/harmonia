7ecd383dd44f146033bbb430f8be0fa597ff520d
owenhochwald <owenhochwald@gmail.com>
2025-09-27 20:29:22 -0700

    change health check location

--- internal/server/health_handler.go
+++ internal/server/health_handler.go
@@ -1,1 +1,9 @@
 package server
+
+import "github.com/gin-gonic/gin"
+
+func (app *Application) HealthHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"status": "healthy - all systems operational",
+	})
+}
--- internal/server/music_handler.go
+++ internal/server/music_handler.go
@@ -1,11 +1,1 @@
 package server
-
-import (
-	"github.com/gin-gonic/gin"
-)
-
-func (app *Application) HealthHandler(c *gin.Context) {
-	c.JSON(200, gin.H{
-		"status": "healthy - all systems operational",
-	})
-}
