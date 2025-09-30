5dc1615b8b15087f94bd0a3d7006a95d0ceba29f
owenhochwald <owenhochwald@gmail.com>
2025-09-30 13:54:46 -0700

    add music service interface and constructor

--- internal/services/music_service.go
+++ internal/services/music_service.go
@@ -8,9 +8,21 @@ import (
 	"github.com/owenhochwald/harmonia/internal/storage"
 )
 
+type MusicServiceInterface interface {
+	HandleUpload(ctx context.Context, song models.Song, data []byte) error
+	Identify(ctx context.Context, hash string) ([]models.Song, error)
+}
+
 type MusicService struct {
 	Storage storage.Storage
 	Repo    repo.SongRepo
+}
+
+func NewMusicService(storage storage.Storage, repo repo.SongRepo) MusicServiceInterface {
+	return &MusicService{
+		Storage: storage,
+		Repo:    repo,
+	}
 }
 
 // TODO: Implement full functionality
