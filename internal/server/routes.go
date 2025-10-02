2d3e42e62b8a1fa6e4e478090097b42806f75ed7
owenhochwald <owenhochwald@gmail.com>
2025-10-02 11:20:44 -0700

    add skeleton song routes

--- internal/server/routes.go
+++ internal/server/routes.go
@@ -5,5 +5,8 @@ import "github.com/gin-gonic/gin"
 func SetupRoutes(r *gin.Engine, app *Application) {
 	r.GET("/health", app.HealthHandler.Check)
 	r.GET("/test-wave-upload", app.MusicHandler.handleTestWaveUpload)
-	r.POST("/music/upload", app.MusicHandler.handleAudioUpload)
+
+	r.POST("/api/upload", app.MusicHandler.handleAudioUpload)
+	r.GET("/api/songs", app.MusicHandler.handleGetSongs)
+	r.GET("/api/songs:id", app.MusicHandler.handleGetASong)
 }
